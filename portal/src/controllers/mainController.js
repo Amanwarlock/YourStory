0d5cba03acf19daadcd7b4fc780a348f57335342
amankareem <aman.kareem@storeking.in>
2018-06-19 09:18:17 +0530

    route validation

--- portal/src/controllers/mainController.js
+++ portal/src/controllers/mainController.js
@@ -7,10 +7,14 @@
 
             $rootScope.$on("$routeChangeStart", function () {
                 self.loggedIn = Auth.isLoggedIn();
-
-                Auth.getUser().then(data => {
-                    self.user = data;
-                });
+                if (self.loggedIn) {
+                    Auth.getUser().then(data => {
+                        self.user = data;
+                    }).catch(e => console.error(e));
+                } else {
+                    console.log("user is not logged in path accessed is : " , $location.path());
+                    $location.path("/login");
+                }
             });
 
             self.doLogin = function () {
@@ -20,7 +24,7 @@
                     Auth.getUser().then(user => {
                         self.user = user;
                     });
-                    
+
                     if (data.success) {
                         $location.path("/");
                     } else {
